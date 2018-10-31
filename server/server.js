a867f77b0c7bd1620a618a7539de0ef334bb7e40
Adam Plante <toomanyadams@gmail.com>
2018-10-30 22:49:29 -0700

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+const app = express();
+
+app.use(express.static(publicPath));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Okay! Listening on port ${port}`);
+});
